ac26d7af8428da7bd5d05e25cf6f0b22addc695a
Aaron Nguyen and Jason Lazzuri <jason@>
2017-05-10 11:02:17 -0700

    initial commit

---
+++ db/migrate/20170510174608_create_ingredients.rb
@@ -0,0 +1,10 @@
+class CreateIngredients < ActiveRecord::Migration[5.1]
+  def change
+    create_table(:ingredients) do |t|
+    t.column(:name, :string)
+    t.column(:recipe_ingredient_id, :integer)
+
+    t.timestamps()
+  end
+  end
+end
